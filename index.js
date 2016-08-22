2598dc8d9ec9efae31eb7b9a6ebd355c5c96d8f5
David Baird <baird_david@bah.com>
2016-08-22 13:59:15 -0600

    Making modifications for serving static pages

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+// These start up express, which handles requests
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res){
-	res.send("<b>Hello World</b>");
+// Direction if a GET Request is preformed for the root of the site
+app.get('/', function(req, res){
+	// Redirect root to index.html
+	res.redirect('/index.html');
     });
 
+// If a user asks for something with a GET Request try to give it to them from the /static_pages/ directory.
+app.get('/:fileName', function (req, res){
+	// Root specifies where to search for the file
+	var options = {
+	    root: __dirname + "/static_pages/"
+	}
+	// Grab the variable fileName in the URI
+	var fileName =  req.params.fileName;
+	// Return to the user the specified file
+	res.sendFile(fileName, options);
+    });
+
+// Start the application listening on port 8080.  In browser localhost:8080/
 app.listen(8080, function () {
 	console.log("Listening on 8080");
     });
