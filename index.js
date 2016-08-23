d838f0fae84399bbdd3a05623bf983b59a23dff5
David Baird <baird_david@bah.com>
2016-08-23 09:25:47 -0600

    Added a basic backend example/proof of concept to allow data entry into the DB. Note this only performs a sample of basic sanitization and no validation.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 // These start up express, which handles requests
 var express = require('express');
+// Parse post body
+var bodyParser = require('body-parser');
+//var multer = require('multer');
+// Actual objects to work with from the above
+//var upload = multer();
 var app = express();
+
+var xss = require('xss');
+
+// Connect to the MongoDB database
+// Currently connects to the Test database
+var db = require('mongoskin').db('mongodb://localhost:27017/Test');
+var dbCollection = 'Test';
+
+// The form types
+var ratingType = "rating";
+var shortAnswerType = "shortAnswer";
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 // Direction if a GET Request is preformed for the root of the site
 app.get('/', function(req, res){
 	// Redirect root to index.html
 	res.redirect('/index.html');
+    });
+
+app.post('/addRating', function (req, res){
+	var dbObject = req.body;
+	dbObject.inputType = ratingType;
+	dbObject.Name = xss(dbObject.Name);
+	db.collection(dbCollection).insert(dbObject);
+	res.send(req.body);
     });
 
 // If a user asks for something with a GET Request try to give it to them from the /static_pages/ directory.
@@ -20,7 +48,9 @@ app.get('/:fileName', function (req, res){
 	res.sendFile(fileName, options);
     });
 
-var db = require('mongoskin').db('mongodb://localhost:27017/Test');
+/*
+This is here a refernce for database calls
+
 db.collection('Test').insert({'name': "Fred", 'colour': "Blue"});
 db.collection('Test').findOne({'name': "Fred"}, function(err, res){
 	if(!err){
@@ -33,6 +63,7 @@ db.collection('Test').findOne({'name': "Fred"}, function(err, res){
 	    console.log(res);
 	}
     });
+*/
 
 // Start the application listening on port 8080.  In browser localhost:8080/
 app.listen(8080, function () {
