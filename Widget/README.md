bb2be40f7cebf2e8449238f17756e07bc88adf30
585158 <sandeshahirtharu@C02S22F0FVH8.local>
2016-08-24 09:51:03 -0400

    Adding the widget code

---
+++ Widget/README.md
@@ -0,0 +1,5 @@
+Instructions for testing the widget
+
+1. Run the command python server.py in the widget folder.
+
+2. Open http://localhost:3003/index.html in browser.
---
+++ Widget/fb.js
@@ -0,0 +1,90 @@
+(function() {
+
+// Localize jQuery variable
+var jQuery;
+
+/******** Load jQuery if not present *********/
+if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.4.2') {
+    var script_tag = document.createElement('script');
+    script_tag.setAttribute("type","text/javascript");
+    script_tag.setAttribute("src",
+        "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+    if (script_tag.readyState) {
+      script_tag.onreadystatechange = function () { // For old versions of IE
+          if (this.readyState == 'complete' || this.readyState == 'loaded') {
+              scriptLoadHandler();
+          }
+      };
+    } else {
+      script_tag.onload = scriptLoadHandler;
+    }
+    // Try to find the head, otherwise default to the documentElement
+    (document.getElementsByTagName("head")[0] || document.documentElement).appendChild(script_tag);
+} else {
+    // The jQuery version on the window is the one we want to use
+    jQuery = window.jQuery;
+    main();
+}
+
+/******** Called once jQuery has loaded ******/
+function scriptLoadHandler() {
+    // Restore $ and window.jQuery to their previous values and store the
+    // new jQuery in our local jQuery variable
+    jQuery = window.jQuery.noConflict(true);
+    // Call our main function
+    main(); 
+}
+
+/******** Our main function ********/
+function main() { 
+    //TODO: get rid of this document.ready stuff
+    jQuery(document).ready(function($) { 
+        /******* Load CSS *******/
+        // var css_link = $("<link>", { 
+        //     rel: "stylesheet", 
+        //     type: "text/css", 
+        //     href: "fb-style.css" 
+        // });
+        // css_link.appendTo('head'); 
+
+        var css1 = $("<link>", { rel: "stylesheet", type: "text/css", href: "fb-style.css" }).appendTo('head');
+        var css2 = $("<link>", { rel: "stylesheet", type: "text/css", href: "bootstrap.css" }).appendTo('head');          
+
+        /******* Load HTML *******/
+        var url = "http://localhost:3003/feedback-form.html";
+        $.get(url,function(data) {
+          $('#fb-container').html(data);
+        });
+    });
+}
+
+// Hides the fb form when a user clicks outside the form
+$(document).mouseup(function (e)
+{
+    var container = $("#widget");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.hide();
+        $("#fb-btn").show();
+
+    }
+});
+
+function initializeFeedback(ele) {
+    //TODO: make ajax request for form.html, and inject to ele
+    //instruct users to add my fb script to their page and call initializeFeedback ot initialize the widget
+}
+
+})(); 
+
+// We call our anonymous function immediately
+
+// var feedback = {
+//     initialize: function(ele) {
+//         //initializationstuff
+//     }
+// }
+
+// feedback.initialize(ele)
