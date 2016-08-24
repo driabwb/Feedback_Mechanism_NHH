127c4af9cd73e7f3ab4041318ab1d1c66405b43c
583546 <allanchua@C02RT2N6FVH8.local>
2016-08-24 15:19:26 -0400

    fixed the click hide bug

--- Widget/fb.js
+++ Widget/fb.js
@@ -62,11 +62,14 @@ function main() {
 $(document).mouseup(function (e)
 {
     var container = $("#widget");
+    var submittedContainer = $("#submittedWidget");
 
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    if ( (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) && (!submittedContainer.is(e.target) // if the target of the click isn't the container...
+        && submittedContainer.has(e.target).length === 0))// ... nor a descendant of the container
     {
         container.hide();
+        submittedContainer.hide();
         $("#fb-btn").show();
 
     }
