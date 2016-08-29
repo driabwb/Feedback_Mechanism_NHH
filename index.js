072d5fbe4c565231ef8a48dcb27f07ccacad9eb9
driabwb <baird_david@bah.com>
2016-08-29 10:57:00 -0400

    Turn strings to numbers when adding ratings

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', function(req, res){
 app.post('/addRating', function (req, res){
 	// Sanitization is expected to be done here validation can be also
 	console.log(req.body);
-	rating = xss(req.body.rating); // Validate that this is a number b/t 1-5
+	rating = parseInt(xss(req.body.rating)); // Validate that this is a number b/t 1-5
 	page = xss(req.body.webpage); // Validate this is one of our pages?
 	db.addRating(rating, page, function(err, result){
 		    if(!err){
